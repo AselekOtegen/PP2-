2a5fb65ae05f1e6d5a541b5db6918e8be70f4046
AselekOtegen <160091391+AselekOtegen@users.noreply.github.com>
2024-02-18 20:14:49 +0600

    Add files via upload

---
+++ TSIS1/NUMBERS.py
@@ -0,0 +1,14 @@
+#EXAMPLE1
+
+x = 5
+x = float(x)
+
+#EXAMPLE2
+
+x=5.5
+x = int(x)
+
+#EXAMPLE3
+
+x=5
+x = complex(x)
---
+++ TSIS1/SYNTAX.py
@@ -0,0 +1,5 @@
+#EXAMPLE1
+print("Hello World")
+#EXAMPLE2
+if 5>2:
+    print("Yes")
