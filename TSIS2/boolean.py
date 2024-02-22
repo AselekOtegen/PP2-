5c4b5d0e9fb073e8646207169995fdc38b6428fd
AselekOtegen <160091391+AselekOtegen@users.noreply.github.com>
2024-02-22 11:59:46 +0600

    Add files via upload

---
+++ TSIS2/boolean.py
@@ -0,0 +1,32 @@
+
+#example1
+'''
+print(10>9)
+
+Output: True
+'''
+#example2
+'''
+print(10 ==9)
+
+Output: False
+'''
+#example3
+
+'''
+print(10<9)
+
+Output: False
+'''
+#example4
+'''
+print(bool("abc"))
+
+Output: True
+'''
+#example5
+'''
+print(bool(0))
+
+Output: False
+'''
