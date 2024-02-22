6748a488253dd78d33247af3738fd3949883f976
AselekOtegen <160091391+AselekOtegen@users.noreply.github.com>
2024-02-22 12:38:23 +0600

    Add files via upload

---
+++ TSIS2/sets.py
@@ -0,0 +1,27 @@
+#example1
+'''
+fruits = {"apple", "banana", "cherry"}
+if "apple" in fruits:
+    print("Yes, apple is a fruit!")
+'''
+#example2
+'''
+fruits = {"apple", "banana", "cherry"}
+fruits.add("orange")
+'''
+#example3
+'''
+fruits = {"apple", "banana", "cherry"}
+more_fruits = ["orange", "mango", "grapes"]
+fruits.update(more_fruits)
+'''
+#example4
+'''
+fruits = {"apple", "banana", "cherry"}
+fruits.remove("banana")
+'''
+#example5
+'''
+fruits = {"apple", "banana", "cherry"}
+fruits.discard("banana")
+'''
---
+++ TSIS2/tuples.py
@@ -0,0 +1,20 @@
+#example1
+'''
+fruits = ("apple", "banana", "cherry")
+print(fruits[0])
+'''
+#example2
+'''
+fruits = ("apple", "banana", "cherry")
+print(len(fruits))
+'''
+#example3
+'''
+fruits = ("apple", "banana", "cherry")
+print(fruits[-1])
+'''
+#example4
+'''
+fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(fruits[2:5])
+'''
