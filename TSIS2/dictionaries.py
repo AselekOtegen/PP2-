2086bfbdda88c996ecc38cb1b6f962c2bdd086b7
AselekOtegen <160091391+AselekOtegen@users.noreply.github.com>
2024-02-22 12:37:15 +0600

    Add files via upload

---
+++ TSIS2/dictionaries.py
@@ -0,0 +1,45 @@
+#example1
+'''
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(car.get("model"))
+'''
+#example2
+'''
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car["year"] = 2020
+'''
+#example3
+'''
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car["color"] = "red"
+'''
+#example4
+'''
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.pop("model")
+'''
+#example5
+'''
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.clear()
+'''
---
+++ TSIS2/forloops.py
@@ -0,0 +1,27 @@
+#example1
+'''
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+'''
+#example2
+'''
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+        continue
+    print(x)
+'''
+#example3
+'''
+for x in range(6):
+    print(x)
+'''
+#example4
+'''
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+        break
+    print(x)
+'''
