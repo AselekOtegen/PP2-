2b65f1968aee5e8d691eda6c5c0c8ef9b9e97543
AselekOtegen <160091391+AselekOtegen@users.noreply.github.com>
2024-02-22 11:58:35 +0600

    Add files via upload

---
+++ TSIS2/lists.py
@@ -0,0 +1,40 @@
+#example1
+'''
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
+'''
+#example2
+'''
+fruits = ["apple", "banana", "cherry"]
+fruits[0] = "kiwi"
+'''
+#example3
+'''
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+'''
+#example4
+'''
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(1,"lemon")
+'''
+#example5
+'''
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("banana")
+'''
+#example6
+'''
+fruits = ["apple", "banana", "cherry"]
+print(fruits[-1])
+'''
+#example7
+'''
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits[2:5])
+'''
+#example8
+'''
+fruits = ["apple", "banana", "cherry"]
+print(len(fruits))
+'''
---
+++ TSIS2/operators.py
@@ -0,0 +1,24 @@
+#example1
+'''
+print(10*5)
+'''
+#example2
+'''
+print(10/2)
+'''
+#example3
+'''
+fruits = ["apple", "banana"]
+if "apple" in fruits:
+    print("Yes, apple is a fruit")
+'''
+#example4
+'''
+if 5!=10:
+    print("5 and 10 is not equal")
+'''
+#example5
+'''
+if 5 == 10 or 4==4:
+    print("At least one of the statements is true")
+'''
